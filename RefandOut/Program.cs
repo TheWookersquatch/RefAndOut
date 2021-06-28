e959dff20ac66bfa2678fbe6c24c6a1cb7d0c8df
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-27 19:03:49 -0600

    initial commit

--- RefandOut/Program.cs
+++ RefandOut/Program.cs
@@ -8,14 +8,26 @@ namespace RefandOut
         //TODO Call the method in the main and make sure the method changes the number of a initialized variable.
         public static string TimesFive(ref int x)
         {
-  
+            int y = x;
+            x *= 5;
+            return $"{y} * 5 = {x} ";
         }
 
         //TODO create method that uses the out keyword. Call the method in the main.
+        public static double SquareRoot(int x, out double root)
+        {
+            return root = Math.Sqrt(x);
+        }
   
         static void Main(string[] args)
         {
-        
+            Console.WriteLine ("What number do you want to multiply by 5?");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{TimesFive(ref input)}");
+            Console.WriteLine($"{TimesFive(ref input)}");
+            double root;
+            SquareRoot(input, out root);
+            Console.WriteLine($"The square root of {input} is {root}");
         }
 
     }
